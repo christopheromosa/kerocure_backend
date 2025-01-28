bedc6654bd09736b196135ff118828e54751ad36
CHRIS922-CODER <christopheromosa632@gmail.com>
2025-01-28 23:26:55 +0300

    patients setup

--- kerocure_medical_center/patients/views.py
+++ kerocure_medical_center/patients/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from .models import Patient, Visit
+from .serializers import PatientSerializer, VisitSerializer
+
+
+class PatientViewSet(ModelViewSet):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+
+
+class VisitViewSet(ModelViewSet):
+    queryset = Visit.objects.all()
+    serializer_class = VisitSerializer
+
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
