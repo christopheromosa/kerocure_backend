91809cec3be802a5d20d59c181b66554add3c216
CHRIS922-CODER <christopheromosa632@gmail.com>
2025-02-21 14:39:05 +0300

    added other patient details

--- kerocure_medical_center/patients/models.py
+++ kerocure_medical_center/patients/models.py
@@ -3,12 +3,18 @@ from django.db import models
 
 # Create your models here.
 class Patient(models.Model):
+    SEX_CHOICES = [
+        ("Male", "Male"),
+        ("Female", "Female"),
+    ]
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+    sex = models.CharField(max_length=6, choices=SEX_CHOICES, default="")
     dob = models.DateField()
+    residence = models.CharField(max_length=255, default="")
     contact_number = models.CharField(max_length=15)
+    next_of_kin_name = models.CharField(max_length=255, default="")
+    next_of_kin_contact_number = models.CharField(max_length=15, default="")
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
-
-
--- kerocure_medical_center/patients/serializers.py
+++ kerocure_medical_center/patients/serializers.py
@@ -1,9 +1,40 @@
 from rest_framework import serializers
 from .models import Patient
+from datetime import date
+
 
 class PatientSerializer(serializers.ModelSerializer):
+
+    age = serializers.SerializerMethodField()
+
     class Meta:
         model = Patient
         fields = "__all__"
 
+    def get_age(self, obj):
+        today = date.today()
+        dob = obj.dob
+        years = (
+            today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+        )
+        # If the patient is less than 1 year old, return age in months and days
+        if years == 0:
+            months = today.month - dob.month
+            days = today.day - dob.day
 
+            # Adjust if the birth month is ahead in the year
+            if months < 0:
+                months += 12
+                years -= 1
+            # Adjust if birth day is ahead in the month
+            if days < 0:
+                from calendar import monthrange
+
+                prev_month_days = monthrange(today.year, today.month - 1)[
+                    1
+                ]  # Days in previous month
+                days += prev_month_days
+                months -= 1
+            return f"{months} months, {days} days" if months > 0 else f"{days} days old"
+        # otherwise, return age in years and months
+        return years
