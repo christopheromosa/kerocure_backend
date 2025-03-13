60b6e3fe0e495635e7ffcec9b71e400f3ef8ad94
CHRIS922-CODER <christopheromosa632@gmail.com>
2025-03-13 15:51:04 +0300

    added staticfiles for django

--- kerocure_medical_center/kerocure_medical_center/settings.py
+++ kerocure_medical_center/kerocure_medical_center/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +28,9 @@ DEBUG = True
 
 
 ALLOWED_HOSTS = ["*"]
+
+
+
 
 
 # Application definition
@@ -161,6 +165,7 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
